136997491754bdd4aa812c77d57a702d990ea332
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-12-06 02:41:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const Glob = require("glob")
+const fs = require("fs")
+const Gradient = require("gradient-string")
+const local = process.env.LOCALAPPDATA
+const installedDiscord = []
+const toCheck = []
+
+var content = fs.readdirSync(local)
+content.forEach(dirContent => {
+    if (dirContent.includes("iscord")) installedDiscord.push(`${local}\\${dirContent}`)
+})
+
+installedDiscord.forEach(r => {
+    Glob.sync(`${r}/app-*/modules/discord_desktop_core-*/discord_desktop_core/index.js`).map(f => toCheck.push(f))
+})
+
+toCheck.forEach(r => {
+    var fileContent = fs.readFileSync(r, 'utf-8')
+    if (fileContent.includes("session")) {
+        console.log(Gradient.instagram(`You've Been Grabbed In \n${r.split("/")[5]}\nI'm removing The Grabber...`))
+        fs.writeFileSync(r, "module.exports = require('./core.asar')")
+        fs.readFile(r, 'utf-8', (err, data) => {
+            if (data.toString() == "module.exports = require('./core.asar')") console.log(Gradient.retro(`Grabber SuccessFully Removed From \n${r}\nPlease Change Your Password`))
+        })
+    } else console.log(Gradient.instagram(`${r.split("/")[5]} is Safe`))
+})
