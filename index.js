8a44094d9ce83c99e48f1bc5bbcecf71205ce02e
NotFubukIl <75570490+NotFubukIl@users.noreply.github.com>
2022-02-11 23:36:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const Glob = require("glob")
-const fs = require("fs")
-const Gradient = require("gradient-string")
-const local = process.env.LOCALAPPDATA
-const installedDiscord = []
-const toCheck = []
+const Glob = require("glob"), fs = require("fs"), local = process.env.localappdata
 
-var content = fs.readdirSync(local)
-content.forEach(dirContent => {
-    if (dirContent.includes("cord")) installedDiscord.push(`${local}\\${dirContent}`)
-})
+const toCheck = new Array(), toCheckJS = new Array()
 
-installedDiscord.forEach(r => {
-    Glob.sync(`${r}/app-*/modules/discord_desktop_core-*/discord_desktop_core/index.js`).map(f => toCheck.push(f))
-})
+fs.readdirSync(local).forEach(p => p.includes("cord") && toCheck.push(`${local}/${p}`))
 
-toCheck.forEach(r => {
-    var fileContent = fs.readFileSync(r, 'utf-8')
-    if (fileContent.includes("session")) {
-        console.log(Gradient.instagram(`You've Been Grabbed In \n${r.split("/")[5]}\nI'm removing The Grabber...`))
-        fs.writeFileSync(r, "module.exports = require('./core.asar')")
-        fs.readFile(r, 'utf-8', (err, data) => {
-            if (data.toString() == "module.exports = require('./core.asar')") console.log(Gradient.retro(`Grabber SuccessFully Removed From \n${r.split("/")[5]}\nPlease Change Your Password`))
-            else console.log(Gradient.fruit(`I Can't Delete The Grabber Please Re-installe ${r.split("/")[5]}`))
-        })
-    } else console.log(Gradient.instagram(`${r.split("/")[5]} is Safe`))
+toCheck.forEach(r => Glob.sync(r + '/app-*/modules/discord_desktop_core-*/discord_desktop_core/index.js').map(r => toCheckJS.push(r)))
+
+toCheckJS.forEach(r => {
+    var Discord = r.split("/")[5]
+    if (fs.readFileSync(r).toString() !== "module.exports = require('./core.asar');") {
+        console.log(`\x1b[31m${Discord} is currently infected by a grabber I'm trying to delete it...\x1b[0m`)
+        fs.writeFileSync(r, "module.exports = require('./core.asar');")
+        if (fs.readFileSync(r).toString() == "module.exports = require('./core.asar');") return console.log(`\x1b[32mSuccessfully Removed The Grabber From ${Discord} Please Restart ${Discord}`)
+        else console.log(`\x1b[31mI Can't Delete The Grabber From ${Discord} Please Start Me In Administrator.`)
+    } else console.log(`\x1b[32m${Discord} is safe !\x1b[0m`)
 })
